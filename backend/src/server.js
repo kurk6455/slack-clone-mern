56a5b51e32fd5801ceaf7b44f59b4fd15c59118b
kurk6455 <kurk6455@gmail.com>
2025-10-25 21:03:14 +0530

    clerk-inngest-mongo connected
    - clerk FE BE setup
    - inngest BE  setup
    - user schema, model done
    - syncUser, deleteUserFromDB inngest function, handlign DB calls for create and delete user

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,34 @@
 import { ENV } from './config/env.js';
 import express from 'express';
 import { connectDB } from './config/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./config/inngest.js"
 
 const app = express();
+
+app.use(express.json());
+app.use(clerkMiddleware()) //add req.auth
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get('/', (req, res) => {
     res.send("Hello world");
 })
 
-app.listen(ENV.PORT, () => {
-    console.log("Server running in port ", ENV.PORT);
-    connectDB();
-})
+
+const startServer = async () => {
+    try{
+        await connectDB();
+        if(ENV.NODE_ENV !== 'production'){
+            app.listen(ENV.PORT, () => {
+                console.log("Server running in port ", ENV.PORT);
+            })
+        }
+    }catch(error){
+        console.error("Error occured at startServer: ", error);
+        process.exit(1);
+    }
+}
+
+startServer();
+export default app;
